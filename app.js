73261b103b77ee7ca7498591434ba11972b38787
Radoslaw Kopec <radek4ec@gmail.com>
2023-01-10 19:40:31 +0100

    Add hosting server

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
+const env = process.env.NODE_ENV || 'development';
+
+app.get('/api/questions', (req, res) => {
+  res.send('Questions from GAPI in the future!')
+})
+
+if(env == 'development') { 
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        next();
+    })
+}
+
+app.use(morgan('dev'));
+
+app.use(express.static(path.join('build')));
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve('build/index.html'));
+});
+
+app.listen(8080, () => { console.log("server started on port 8080") });
