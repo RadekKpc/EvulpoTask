0734a3f9fc1c52234fc4c366c2d8b47b1610e8c8
Radoslaw Kopec <radek4ec@gmail.com>
2023-01-12 19:58:08 +0100

    Add documentation for Quiz class

--- client/utils/Quiz.js
+++ client/utils/Quiz.js
@@ -1,9 +1,24 @@
+/**
+ * The complete question
+ * @typedef {Object} Question
+ * @property {string} id - unique indentifier
+ * @property {string} topic - the category of the question like "Math","Geography" etc.
+ * @property {string} question - the question itself
+ * @property {string[]} options - available answers for question
+ * @property {number} correct_answer - index of correct answer in options array
+ * @property {number} score - indicates how difficult the question is
+ */
+
 export default class Quiz {
     constructor() {
         this.questions = [];
         this.selectedAnswers = [];
     }
 
+    /**
+     * Set up question, and initialize all answers to not selected
+     * @param {Question} questions - The part of quize
+     */
     setQuestions(questions) {
         this.questions = questions;
         // -1 means not selected answer
